39686a466153ec8947284950f6e858200bf3b132
drobertshaw01 <94071376+drobertshaw01@users.noreply.github.com>
2022-06-03 15:13:26 +0100

    Files from 1.2.5 activity

---
+++ 1.2.5 my_first_code.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# this is my first line of code
+
+print("my name is David")
+print("the date is June 3rd 2022")
+
+
+# In[2]:
+
+
+# Write a program to analyse real estate data
+
+# create variables - sales price by property for December
+a = 45000
+b = 23400
+c = 67000
+d = 34600
+e = 12900
+
+
+# In[4]:
+
+
+# total sales for December
+total_sales = (a + b + c + d + e)
+print(total_sales)
+
+
+# In[5]:
+
+
+# average sales for December
+print(total_sales/6)
+
+
+# In[6]:
+
+
+# cal property price for C
+print(c - 6500)
+
+
+# In[ ]:
+
+
+
+
